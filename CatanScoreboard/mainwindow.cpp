dcb1f1730c99fe6810603e4a4501efaaec713aa1
Cedric <cedric.dewijs@telfort.nl>
2015-08-30 06:08:57 +0000

    The events of the buttons work as expected on a tablet

--- CatanScoreboard/mainwindow.cpp
+++ CatanScoreboard/mainwindow.cpp
@@ -18,6 +18,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_tabWidget_currentChanged(int index)
 {
+    ui->textBrowser->append("on_tabWidget_currentChanged");
     qDebug("index: %d",index);
     switch (index)
     {
@@ -41,10 +42,72 @@ void MainWindow::on_btnPlayersMin_pressed()
 {
     myNumPlayers--;
     ui->lcdNumPlayers->display(myNumPlayers);
+    ui->textBrowser->append("on_btnPlayersMin_pressed");
 }
 
 void MainWindow::on_btnPlayersPlus_pressed()
 {
     myNumPlayers++;
     ui->lcdNumPlayers->display(myNumPlayers);
+    ui->textBrowser->append("on_btnPlayersPlus_pressed");
+}
+
+void MainWindow::on_toolButton_clicked()
+{
+    ui->textBrowser->append("on_toolButton_clicked");
+}
+
+void MainWindow::on_toolButton_clicked(bool checked)
+{
+    ui->textBrowser->append("on_toolButton_clicked(bool checked)");
+}
+
+void MainWindow::on_toolButton_pressed()
+{
+     ui->textBrowser->append("on_toolButton_pressed()");
+}
+
+void MainWindow::on_toolButton_released()
+{
+    ui->textBrowser->append("on_toolButton_released()");
+}
+
+void MainWindow::on_toolButton_toggled(bool checked)
+{
+    ui->textBrowser->append("on_toolButton_toggled(bool checked)");
+}
+
+void MainWindow::on_toolButton_destroyed()
+{
+    ui->textBrowser->append("on_toolButton_destroyed()");
+}
+
+void MainWindow::on_toolButton_destroyed(QObject *arg1)
+{
+    ui->textBrowser->append("on_toolButton_destroyed(QObject *arg1)");
+}
+
+void MainWindow::on_toolButton_objectNameChanged(const QString &objectName)
+{
+    ui->textBrowser->append("on_toolButton_objectNameChanged(const QString &objectName)");
+}
+
+void MainWindow::on_toolButton_triggered(QAction *arg1)
+{
+    ui->textBrowser->append("on_toolButton_triggered(QAction *arg1)");
+}
+
+void MainWindow::on_toolButton_customContextMenuRequested(const QPoint &pos)
+{
+    ui->textBrowser->append("on_toolButton_customContextMenuRequested(const QPoint &pos)");
+}
+
+void MainWindow::on_toolButton_windowIconTextChanged(const QString &iconText)
+{
+    ui->textBrowser->append("on_toolButton_windowIconTextChanged(const QString &iconText)");
+}
+
+void MainWindow::on_toolButton_windowTitleChanged(const QString &title)
+{
+    ui->textBrowser->append("on_toolButton_windowTitleChanged(const QString &title)");
 }
--- CatanScoreboard/mainwindow.h
+++ CatanScoreboard/mainwindow.h
@@ -20,6 +20,30 @@ private slots:
     void on_btnPlayersMin_pressed();
     void on_btnPlayersPlus_pressed();
 
+    void on_toolButton_clicked();
+
+    void on_toolButton_clicked(bool checked);
+
+    void on_toolButton_pressed();
+
+    void on_toolButton_released();
+
+    void on_toolButton_toggled(bool checked);
+
+    void on_toolButton_destroyed();
+
+    void on_toolButton_destroyed(QObject *arg1);
+
+    void on_toolButton_objectNameChanged(const QString &objectName);
+
+    void on_toolButton_triggered(QAction *arg1);
+
+    void on_toolButton_customContextMenuRequested(const QPoint &pos);
+
+    void on_toolButton_windowIconTextChanged(const QString &iconText);
+
+    void on_toolButton_windowTitleChanged(const QString &title);
+
 private:
     Ui::MainWindow *ui;
     int myNumPlayers;
